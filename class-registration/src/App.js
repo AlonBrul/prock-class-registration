21f380f75764175ea93a88b783d2e50739ff9d5e
Alon <alomAfeka@gmail.com>
2024-03-03 13:33:48 +0200

    created react app and moved index.html and style.css content into App.js and App.css

---
+++ class-registration/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div class="dashboard">
+        <button class="menu-btn fa-solid fa-bars"></button>
+      </div>
+
+      <div class="user">דניאל מלכיאלוביץ</div>
+
+      <div class="class"></div>
+    </div>
+  );
+}
+
+export default App;
